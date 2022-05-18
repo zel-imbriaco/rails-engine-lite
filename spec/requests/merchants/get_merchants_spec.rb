d31522a509c0a1c510541934b0d3ceee4fe842c8
Zachary Imbriaco <zachary@imbriaco.com>
2022-05-18 12:55:17 -0400

    Feat: Happy path testing for /merchants/:id passes.

--- spec/requests/merchants/get_merchants_spec.rb
+++ spec/requests/merchants/get_merchants_spec.rb
@@ -12,6 +12,13 @@ RSpec.describe 'Merchants', type: :request do
       expect(json["data"].size).to eq 10 
     end
 
+    it 'Has expected attributes for merchants' do
+      expect(json["data"].first["type"]).to eq "merchant"
+      expect(json["data"].last["type"]).to eq "merchant"
+      expect(json["data"].first["attributes"]).to have_key("name")
+      expect(json["data"].last["attributes"]).to have_key("name")
+    end
+
     it 'returns 200 status code' do
       expect(response).to have_http_status :success
     end
@@ -20,17 +27,19 @@ RSpec.describe 'Merchants', type: :request do
   describe 'GET /merchants/:id' do
     before do
       FactoryBot.create_list(:merchant, 10)
+      @merchant1 = Merchant.create!(id: 1, name: "Zel")
+      get "/api/v1/merchants/1"
     end
 
     it 'Returns 200 status on valid request' do
-      get "/api/v1/merchants/1"
       expect(response).to have_http_status :success
     end
 
-    it 'Returns 404 status on invalid merchant id' do
-      get "/api/v1/merchants/413"
+    it 'Returns merchant data' do
       binding.pry
-      expect(response).to have_http_status :failure
-    end
+      expect(json["data"]["type"]).to eq "merchant"
+      expect(json["data"]["attributes"]).to have_key "name"
+      expect(json["data"]["attributes"]["name"]).to eq "Zel"
+    end 
   end
 end
