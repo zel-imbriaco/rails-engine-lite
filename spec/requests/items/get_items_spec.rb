44b76237525724791bc7a95191baf359a32704cb
Zachary Imbriaco <zachary@imbriaco.com>
2022-05-18 16:35:44 -0400

    Feat: GET /items/:id passes happy path spec.

--- spec/requests/items/get_items_spec.rb
+++ spec/requests/items/get_items_spec.rb
@@ -20,10 +20,30 @@ RSpec.describe 'Items', type: :request do
       expect(json["data"].first["attributes"]).to have_key "name"
       expect(json["data"].first["attributes"]).to have_key "description"
       expect(json["data"].first["attributes"]).to have_key "unit_price"
+      expect(json["data"].first["type"]).to eq "item"
       expect(json["data"].last["attributes"]).to have_key "name"
       expect(json["data"].last["attributes"]).to have_key "description"
       expect(json["data"].last["attributes"]).to have_key "unit_price"
+      expect(json["data"].last["type"]).to eq "item"
     end
   end
 
+  describe 'GET /items/:id' do
+    before do
+      @item = create(:item, id: 10)
+      get '/api/v1/items/10'
+    end
+
+    it 'Returns 200 response' do
+      expect(response).to have_http_status :success
+    end
+
+    it 'Returns the item' do
+      expect(json["data"]["attributes"]).to have_key "name"
+      expect(json["data"]["attributes"]).to have_key "description"
+      expect(json["data"]["attributes"]).to have_key "unit_price"
+      expect(json["data"]["type"]).to eq "item"
+      expect(json["data"]["id"]).to eq "10"
+    end
+  end
 end
